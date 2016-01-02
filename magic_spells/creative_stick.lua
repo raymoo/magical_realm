d3882a494fa937ac5aae9d0a247fb5a3b7e27c41
Raymoo <uguu@installgentoo.com>
2016-01-01 17:44:22 -0800

    Nicer stick interface

--- magic_spells/creative_stick.lua
+++ magic_spells/creative_stick.lua
@@ -4,43 +4,73 @@ local wand_range = tonumber(minetest.setting_get("magic_creative_wand_range")) o
 
 local texture = "default_mese_crystal_fragment.png^default_stick.png"
 
-local selected_sticks = {}
-
 local selected_spells = {}
 
-local f_name = "magic:creative_stick"
+local stick_form = smartfs.create("magic_spells:creative_stick", function(state)
 
-local form = "field[select;Spell: ;]"
+	local p_name = state.player
+	local prepareds = state.param
+	local displays = {}
 
+	local current_s_name = selected_spells[p_name]
 
-local function handle_fields(player, formname, fields)
+	local current_displayed
 
-	if (formname ~= f_name) then return false end
+	if (current_s_name == nil) then
+		current_displayed = "None"
+	else
+		local spell = magic_spells.registered_spells[current_s_name]
+
+		if (spell == nil) then
+			current_displayed = "Invalid Spell"
+		else
+			current_displayed = spell.display_name
+		end
+	end
+		
+
+	state:size(8,8)
+	local cur_lab = state:label(0.5,0.5, "title", "Current Spell: " .. current_displayed)
+	local select_box = state:listbox(0.5,1, 7,5.5, "prepareds")
+
+	state:button(0.5,7, 2,1, "done", "Done", true)
+
+	select_box:removeItem(1)
 	
-	local p_name = player:get_player_name()
-	
-	local stick = selected_sticks[p_name]
+	for i, s_name in ipairs(prepareds) do
 
+		local spell = magic_spells.registered_spells[s_name]
 
-	if (stick == nil) then
-		minetest.chat_send_player(p_name, "Error, no associated stick")
-		return true
+		if (spell == nil) then
+			select_box:addItem("Invalid Spell")
+		else
+			select_box:addItem(spell.display_name)
+		end
 	end
 
+	select_box:onClick(function(self, state, idx)
 
-	if (fields.select == nil) then return true end
+			current_s_name = prepareds[tonumber(idx)]
 
+			local spell = magic_spells.registered_spells[current_s_name]
 
-	selected_spells[p_name] = fields.select
+			selected_spells[p_name] = current_s_name
 
-	minetest.chat_send_player(p_name, "Selected " .. fields.select)
+			if (current_s_name == nil) then
+				current_displayed = "Nothing"
+			elseif (spell == nil) then
+				current_displayed = "Invalid Spell"
+			else
+				current_displayed = spell.display_name
+			end
 
-	return true
+			cur_lab:setText("Current Spell: " .. current_displayed)
+			
+			state:show()
 
-end
-
-
-minetest.register_on_player_receive_fields(handle_fields)
+	end)
+end)
+		
 
 
 local function show_stick(stack, player, pointed_thing)
@@ -49,9 +79,14 @@ local function show_stick(stack, player, pointed_thing)
 
 	local p_name = player:get_player_name()
 
-	selected_sticks[p_name] = stack
+	local prepareds = {}
 
-	minetest.show_formspec(p_name, f_name, form)
+	for s_name, def in pairs(magic_spells.registered_spells) do
+
+		table.insert(prepareds, s_name)
+	end
+
+	stick_form:show(p_name, prepareds)
 	
 	return nil
 end
