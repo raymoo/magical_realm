63a39f4e628b30faf30edd21c2eae618a0653242
Raymoo <uguu@installgentoo.com>
2016-01-31 15:30:27 -0800

    Add book category to spellbook loot

--- combat_spells/init.lua
+++ combat_spells/init.lua
@@ -6,14 +6,14 @@ dofile(mod_path .. "reflect_lesser.lua")
 dofile(mod_path .. "rock_hide.lua")
 
 
-loot.register_loot({ weights = { generic = 20, magical = 100 },
+loot.register_loot({ weights = { generic = 20, magical = 100, book = 100 },
 		     payload = { stack = spellbooks.spellbook("magic_missile") },
 })
 
-loot.register_loot({ weights = { generic = 10, magical = 50 },
+loot.register_loot({ weights = { generic = 10, magical = 50, book = 50 },
 		     payload = { stack = spellbooks.spellbook("reflect_lesser") },
 })
 
-loot.register_loot({ weights = { generic = 10, magical = 50 },
+loot.register_loot({ weights = { generic = 10, magical = 50, book = 50 },
 		     payload = { stack = spellbooks.spellbook("rock_hide") },
 })
--- travel_spells/init.lua
+++ travel_spells/init.lua
@@ -6,19 +6,19 @@ dofile(mod_path .. "moon_shoes.lua")
 dofile(mod_path .. "haste.lua")
 
 
-loot.register_loot({ weights = { generic = 20, magical = 100 },
+loot.register_loot({ weights = { generic = 20, magical = 100, book = 100 },
 		     payload = { stack = spellbooks.spellbook("blink") },
 })
 
-loot.register_loot({ weights = { generic = 4, magical = 20 },
+loot.register_loot({ weights = { generic = 4, magical = 20, book = 20 },
 		     payload = { stack = spellbooks.spellbook("ethereal_jaunt") },
 })
 
-loot.register_loot({ weights = { generic = 20, magical = 100 },
+loot.register_loot({ weights = { generic = 20, magical = 100, book = 100 },
 		     payload = { stack = spellbooks.spellbook("moon_shoes") },
 })
 
-loot.register_loot({ weights = { generic = 10, magical = 50 },
+loot.register_loot({ weights = { generic = 10, magical = 50, book = 50 },
 		     payload = { stack = spellbooks.spellbook("haste") },
 })
 
