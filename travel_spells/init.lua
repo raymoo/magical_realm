8054fcb8105961469f38bd71a2f451618354e4ec
Raymoo <uguu@installgentoo.com>
2016-01-30 23:00:00 -0800

    Add spellbooks as loot for travel spells

--- travel_spells/init.lua
+++ travel_spells/init.lua
@@ -4,3 +4,21 @@ dofile(mod_path .. "blink.lua")
 dofile(mod_path .. "ethereal_jaunt.lua")
 dofile(mod_path .. "moon_shoes.lua")
 dofile(mod_path .. "haste.lua")
+
+
+loot.register_loot({ weights = { generic = 20, magical = 100 },
+		     payload = { stack = spellbooks.spellbook("blink") },
+})
+
+loot.register_loot({ weights = { generic = 4, magical = 20 },
+		     payload = { stack = spellbooks.spellbook("ethereal_jaunt") },
+})
+
+loot.register_loot({ weights = { generic = 20, magical = 100 },
+		     payload = { stack = spellbooks.spellbook("moon_shoes") },
+})
+
+loot.register_loot({ weights = { generic = 10, magical = 50 },
+		     payload = { stack = spellbooks.spellbook("haste") },
+})
+
