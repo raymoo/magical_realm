ecc17e085fbaedc5aeead3eea5701c041b5688c1
Raymoo <uguu@installgentoo.com>
2016-01-30 23:00:33 -0800

    Add combat_spells spellbooks as loot

--- combat_spells/init.lua
+++ combat_spells/init.lua
@@ -4,3 +4,16 @@ dofile(mod_path .. "summon_tnt.lua")
 dofile(mod_path .. "magic_missile.lua")
 dofile(mod_path .. "reflect_lesser.lua")
 dofile(mod_path .. "rock_hide.lua")
+
+
+loot.register_loot({ weights = { generic = 20, magical = 100 },
+		     payload = { stack = spellbooks.spellbook("magic_missile") },
+})
+
+loot.register_loot({ weights = { generic = 10, magical = 50 },
+		     payload = { stack = spellbooks.spellbook("reflect_lesser") },
+})
+
+loot.register_loot({ weights = { generic = 10, magical = 50 },
+		     payload = { stack = spellbooks.spellbook("rock_hide") },
+})
