d959d59122924039e0809b245ffb74d6067aab76
Raymoo <uguu@installgentoo.com>
2016-01-31 17:39:19 -0800

    excavate spellbook spawns as loot

--- build_spells/init.lua
+++ build_spells/init.lua
@@ -1,3 +1,8 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
 
 dofile(modpath .. "excavate.lua")
+
+
+loot.register_loot({ weights = { generic = 16, magical = 80, book = 80 },
+		     payload = { stack = spellbooks.spellbook("excavate") },
+})
